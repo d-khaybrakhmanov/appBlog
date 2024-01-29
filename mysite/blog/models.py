22bbed5a55a8b9bdcb139c8bb1ff99c3369151f2
d.khaybrakhmanov <d.khaybrakhmanov@yandex.ru>
2024-01-29 22:17:47 +0300

    canonic url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
 
@@ -24,6 +28,8 @@ class Post(models.Model):
                               choices=Status.choices,
                               default=Status.DRAFT)
 
+    object = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
