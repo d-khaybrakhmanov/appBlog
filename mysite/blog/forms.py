8d91226782932dd3232fe99b22a19147d66b4627
d.khaybrakhmanov <d.khaybrakhmanov@yandex.ru>
2024-01-30 18:45:39 +0300

    Add email form

---
+++ mysite/blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.CharField(max_length=25)
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'd.khaybrakhmanov@icloud.com'
+EMAIL_HOST_PASSWORD = 'tmfbepptwjudthzl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
